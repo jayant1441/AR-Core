05d73e42294eb08f728ad1ef674f838174b513ed
Jayant Dhingra <jayantdhingra1441@gmail.com>
2020-03-23 16:58:17 +0530

    App made in one go

---
+++ app/src/main/java/com/jayantx/arcore/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.jayantx.arcore
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var arFragment: ArFragment
+    lateinit var selectedObject: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //Init Fragment
+        arFragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment_view) as ArFragment
+
+        //Default model
+        setModelPath("pickachu.sfb")
+
+        //Tab listener for the ArFragment
+        arFragment.setOnTapArPlaneListener { hitResult, plane, _ ->
+            //If surface is not horizontal and upward facing
+            if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                //return for the callback
+                return@setOnTapArPlaneListener
+            }
+            //create a new anchor
+            val anchor = hitResult.createAnchor()
+            placeObject(arFragment, anchor, selectedObject)
+        }
+
+        //Click listener for lamp and table objects
+
+
+        pickachu.setOnClickListener {
+            setModelPath("pickachu.sfb")
+        }
+        ironman.setOnClickListener {
+            setModelPath("ironman.sfb")
+        }
+        cybertruck.setOnClickListener {
+            setModelPath("cybertruck.sfb")
+        }
+        cobra.setOnClickListener {
+            setModelPath("cobra.sfb")
+        }
+
+    }
+
+
+
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, renderableObject: Renderable) {
+        val anchorNode = AnchorNode(anchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderableObject
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, modelUri: Uri) {
+        val modelRenderable = ModelRenderable.builder()
+            .setSource((fragment.requireContext()), modelUri)
+            .build()
+
+        //when the model render is build add node to scene
+        modelRenderable.thenAccept { renderableObject -> addNodeToScene(fragment, anchor, renderableObject) }
+
+        //handle error
+        modelRenderable.exceptionally {
+            Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
+            null
+        }
+    }
+
+    private fun setModelPath(modelFileName: String) {
+        selectedObject = Uri.parse(modelFileName)
+        val toast = Toast.makeText(applicationContext, modelFileName, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+
+}
